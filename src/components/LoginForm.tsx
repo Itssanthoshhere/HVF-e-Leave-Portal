f3d08caff64ead9162958280bc48d263f98dd542
Itssanthoshhere <santhosh02vs@gmail.com>
2025-06-27 02:20:04 +0530

    Initial Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -99,7 +99,7 @@ const LoginForm = ({ onLogin, onSignUp, isLoading }: LoginFormProps) => {
           <div className="flex justify-center mb-4">
             <div className="bg-white p-4 rounded-full shadow-lg">
               <img
-                src="public/images/hvf.png"
+                src="images/hvf.png"
                 alt="Heavy Vehicles Factory Logo"
                 className="h-16 w-16 object-contain"
               />
